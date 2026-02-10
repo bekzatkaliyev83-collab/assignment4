7f13943650722dd34635f6cb4ec16f60455de414
bekzatkaliyev83-collab <bekzatkaliyev83@gmail.com>
2026-02-10 15:20:20 +0600

    Update DatabaseTest.java

--- main/java/DatabaseTest.java
+++ main/java/DatabaseTest.java
@@ -12,11 +12,11 @@ public class DatabaseTest {
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("PostgreSQL-ге қосылдық! ✅");
-            System.out.println("Дерекқор нұсқасы: " + conn.getMetaData().getDatabaseProductVersion());
+            System.out.println();
+            System.out.println(conn.getMetaData().getDatabaseProductVersion());
 
         } catch (SQLException e) {
-            System.out.println("Қосылу қатесі шықты:");
+            System.out.println();
             System.out.println(e.getMessage());
             e.printStackTrace();
         } finally {
